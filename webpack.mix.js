99b51cfe086e8be870346eee03b548174e089332
Byronn Walker-Mills <bwalkermills@gmail.com>
2021-11-17 22:19:30 +0000

    Fix relative image paths in css

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,7 @@ let mix = require('laravel-mix');
 
 mix
     .js('src/js/app.js', 'js')
-    .sass('src/scss/app.scss', 'css');
+    .sass('src/scss/app.scss', 'css')
+    .options({
+        processCssUrls: false
+    });
